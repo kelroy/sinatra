43527c9b9245fcc71ee27c1df62f35b0a5fbc863
test <kelroy@ubuntu.(none)>
2011-01-28 10:15:22 -0800

    xml routing

--- test.rb
+++ test.rb
@@ -1,18 +1,28 @@
 require 'rubygems'
 require 'sinatra'
 require 'haml'
+require 'json'
+
 
 get '/' do
   "Hello World!"
 end
 
-get '/haml/?' do
+get '/hello_haml/?' do
   haml :haml, :layout => :layout
 end
 
 get '/hello_sass/?' do
-
   sass :sass
-
 end
 
+get '/json/?' do
+  content_type :text
+  { :key1 => 'value1', :key2 => 'value2' }.to_json
+end
+
+get "/xml/?" do
+  
+  haml(:xml,:layout => false)
+ 	
+end
